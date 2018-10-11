1c3db164aeb9cad3f6164c49973cba14bc4949b2
Sophie Gu <sophieqgu@gmail.com>
2018-10-11 15:04:59 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -24,7 +24,8 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  shipping_manifest[""]
+  shipping_manifest["muskets"] => 2 
+  shipping_manifest[]
 
 
 
