e81c343099401d8c33a77602ca4105de961954a9
Sophie Gu <sophieqgu@gmail.com>
2018-10-11 15:05:36 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -25,14 +25,6 @@ def adding
                   "oil paintings" => 3
                  }
   shipping_manifest["muskets"] => 2 
-  shipping_manifest[]
-
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-
+  shipping_manifest["gun powder"] => 4
+  shipping_manifest
 end
